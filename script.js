f86b83240bb92bccac9e481bb41747765fbd0651
Kevin_Isaza <2089276@talnet.nl>
2020-01-24 13:31:02 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+const pi = 3.14;
+
+function calculate() {
+  let diameter = document.getElementById("diameter").value;
+
+  let resultPeriphery = diameter * pi;
+  let PeripherySpan = document.getElementById("resultPeriphery");
+
+  PeripherySpan.innerText = resultPeriphery;
+
+  let resultSurface = diameter * diameter * pi * 0.25;
+  let surfaceSpan = document.getElementById("resultSurface");
+  surfaceSpan.innerText = resultSurface;
+}
